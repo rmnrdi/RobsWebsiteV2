5b73cdc382982b9bb87513f15757abbc169bbbf2
Rob Minardi <robmcec@gmail.com>
2019-01-27 19:21:28 -0500

    Revert "Deleted Power and Base Curve models"
    This reverts commit 5df25b8056ea689ca2efe2537034d693560a7406.

---
+++ RobsWebsiteV2/Models/BaseCurveModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RobsWebsiteV2.Models
+{
+    public class BaseCurveModel
+    {
+        [Required]
+        [Range(0, 30)]
+        public double SpherePower { get; set; }
+
+        [Required]
+        [Range(-30, -0)]
+        public double CylinderPower { get; set; }
+       
+        [Range(1, 180)]
+        public double? CylAxis { get; set; }
+
+        [Range(0, 5)]
+        public double? AddPower { get; set; }
+
+        [Required]
+        public double Result { get; set; }
+
+    }
+}
---
+++ RobsWebsiteV2/Models/PowerModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RobsWebsiteV2.Models
+{
+    public class PowerModel
+    {
+        [Required]
+        [Range(0,30)]
+        public  double PowerFront { get; set; }
+
+        [Required]
+        [Range(-30,0)]
+        public  double PowerBack { get; set; }
+
+        [Required]
+        public  double Result { get; set; }
+
+        
+    }
+}
