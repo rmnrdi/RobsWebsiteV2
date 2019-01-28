db16943fe0b0305e7ba19f219f0d4740174b2dcd
Rob Minardi <robmcec@gmail.com>
2019-01-27 20:29:15 -0500

    Fixed Routes

--- RobsWebsiteV2/Areas/API/Controllers/PrismController.cs
+++ RobsWebsiteV2/Areas/API/Controllers/PrismController.cs
@@ -28,9 +28,9 @@ namespace RobsWebsiteV2.Areas.API.Controllers
         /// <param name="prism"></param>
         /// <returns></returns>
         [HttpGet("ApicalAngle/{DegreesDeviation}/{Index}")]
-        public ActionResult<PrismModel> ApicalAngle(PrismModel prism)
+        public ActionResult<ApicalAngleModel> ApicalAngle(ApicalAngleModel prism)
         {
-            prism.Result = Prism.ApicalAngle(prism.DegreesDeviation, prism.Index);
+            prism.Result = Prism.ApicalAngle(prism.Deviation, prism.Index);
 
             return prism;
         }
@@ -38,49 +38,49 @@ namespace RobsWebsiteV2.Areas.API.Controllers
         [HttpGet("PrismDiopter/{Displacement}/{Distance}")]
         public ActionResult<PrismDiopterModel> PrismDiopter(PrismDiopterModel prism)
         {
-            prism.Result = Prism.PrismDiopter(prism.Displacement,prism.Distance);
+            prism.Result = Prism.PrismDiopter(prism.Displacement, prism.Distance);
 
             return prism;
         }
 
-        public ActionResult<PrismDisplacementModel> PrismDisplacement(PrismDisplacementModel prism)
-        {
-            prism.Result = Prism.PrismDisplacement(prism.PrismDiopters, prism.Distance);
+            //public ActionResult<PrismDisplacementModel> PrismDisplacement(PrismDisplacementModel prism)
+            //{
+            //    prism.Result = Prism.PrismDisplacement(prism.PrismDiopters, prism.Distance);
 
-            return prism;
+            //    return prism;
+            //}
+
+            //[HttpGet("PrismCentrad/{DeviationAngle}")]
+            //public ActionResult<double> PrismCentrad(PrismModel prism)
+            //{
+            //    if (!ModelState.IsValid)
+            //        return BadRequest(ModelState);
+
+            //    prism.Result = Prism.PrismCentrad(prism.DeviationAngle);
+
+            //    return prism.Result;
+            //}
+
+            //[HttpGet("PrenticesRuleCentimeters/{LensPower}/{Decentration}")]
+            //public ActionResult<PrenticesRuleModel> PrenticesRuleCentimeters(PrenticesRuleModel prism)
+            //{
+            //    if (!ModelState.IsValid)
+            //        return BadRequest(ModelState);
+
+            //    prism.Result = Prism.PrenticesLawCentimeters(prism.LensPower, prism.Decentration);
+
+            //    return prism;
+            //}
+
+            //[HttpGet("PrenticesRuleMillimeters/{LensPower}/{Decentration}")]
+            //public ActionResult<PrenticesRuleModel> PrenticesRuleMillimeters(PrenticesRuleModel prism)
+            //{
+            //    if (!ModelState.IsValid)
+            //        return BadRequest(ModelState);
+
+            //    prism.Result = Prism.PrenticesLawMillimeters(prism.LensPower, prism.Decentration);
+
+            //    return prism;
+            //}
         }
-
-        [HttpGet("PrismCentrad/{DeviationAngle}")]
-        public ActionResult<double> PrismCentrad(PrismModel prism)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            prism.Result = Prism.PrismCentrad(prism.DeviationAngle);
-
-            return prism.Result;
-        }
-
-        [HttpGet("PrenticesRuleCentimeters/{LensPower}/{Decentration}")]
-        public ActionResult<PrenticesRuleModel> PrenticesRuleCentimeters(PrenticesRuleModel prism)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            prism.Result = Prism.PrenticesLawCentimeters(prism.LensPower, prism.Decentration);
-
-            return prism;
-        }
-
-        [HttpGet("PrenticesRuleMillimeters/{LensPower}/{Decentration}")]
-        public ActionResult<PrenticesRuleModel> PrenticesRuleMillimeters(PrenticesRuleModel prism)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            prism.Result = Prism.PrenticesLawMillimeters(prism.LensPower, prism.Decentration);
-
-            return prism;
-        }
-    }
 }
--- RobsWebsiteV2/CalcModels/Prism/PrismDiopterModel.cs
+++ RobsWebsiteV2/CalcModels/Prism/PrismDiopterModel.cs
@@ -1,10 +1,13 @@
-﻿namespace RobsWebsiteV2.Areas.API.Controllers
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RobsWebsiteV2.Areas.API.Controllers
 {
     public class PrismDiopterModel
     {
         public double Displacement { get; set; }
         public double Distance { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:0.000}")]
         public double Result { get; set; }
     }
 }
