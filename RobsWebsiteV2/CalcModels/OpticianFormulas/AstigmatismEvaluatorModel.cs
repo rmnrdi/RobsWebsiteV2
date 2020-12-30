e7dc4eb5daaa098b4ed72c1c00c910feb3e668d4
rmnrdi <robmopti@gmail.com>
2020-12-30 12:02:30 -0500

    Changed

---
+++ RobsWebsiteV2/CalcModels/OpticianFormulas/AstigmatismEvaluatorModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RobsWebsiteV2.CalcModels.OpticianFormulas
+{
+    public class AstigmatismEvaluatorModel
+    {
+        [Required]
+        [Range(-30,30)]
+        public double SpherePower { get; set; }
+
+        [Required]
+        [Range(-30, 0)]
+        public double CylinderPower { get; set; }
+
+    }
+}
---
+++ RobsWebsiteV2/CalcModels/OpticianFormulas/BinocularDecentrationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RobsWebsiteV2.CalcModels.OpticianFormulas
+{
+    public class BinocularDecentrationModel
+    {
+        public double AMeasurement { get; set; }
+
+        public double DBLMeasurement { get; set; }
+
+        public double MonoPD{ get; set; }
+    }
+}
