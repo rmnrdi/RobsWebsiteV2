df97eaa0bcfd2b5febba94229637ddb56b67e1a1
rmnrdi <robmopti@gmail.com>
2021-01-01 13:54:42 -0500

    Added tool selector controller

---
+++ RobsWebsiteV2/Areas/API/Controllers/ToolSelectorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RobsWebsiteV2.CalcModels.ToolSelector;
+using OpticianMathLibrary;
+
+namespace RobsWebsiteV2.Areas.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [Produces("application/json")]
+    public class ToolSelectorController : Controller
+    {
+        [HttpGet("RefractivePower/{ToolPower}/{Index}")]
+        public ActionResult<RefractivePowerModel> RefractivePower(RefractivePowerModel power)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            power.Result = Tools.RefractivePower(power.ToolPower, power.Index);
+            return power;
+        }
+
+        [HttpGet("ToolSelector/{RefractivePower}/{Index}")]
+        public ActionResult<ToolSelectorModel> ToolSelector(ToolSelectorModel tool)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            tool.Result = Tools.ToolSelector(tool.RefractivePower, tool.Index);
+            return tool;
+        }
+    }
+}
