25b0cee7fedcc739d4ca03a507f5c279112b907f
rmnrdi <robmopti@gmail.com>
2021-01-10 16:34:45 -0500

    Added Magnification controller

--- RobsWebsiteV2/Areas/API/Controllers/MagnificationController.cs
+++ RobsWebsiteV2/Areas/API/Controllers/MagnificationController.cs
@@ -1,13 +1,21 @@
-﻿namespace RobsWebsiteV2.Areas.api.Controllers
-{
-    //[Route("api/v1/[controller]")]
-    //[Produces("application/json")]
+﻿using Microsoft.AspNetCore.Mvc;
+using RobsWebsiteV2.CalcModels.Magnification;
+using OpticianMathLibrary;
 
-    //public class MagnificationController : Controller
-    //{
-    //    public IActionResult Index()
-    //    {
-    //        return View();
-    //    }
-    //}
+namespace RobsWebsiteV2.Areas.api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [Produces("application/json")]
+    public class MagnificationController : Controller
+    {
+        [HttpGet("SpectacleMagnification/{FrontBaseCurve}/{ActualLensPower}/{Index}/{Thickness}/{VertexDistance}")]        
+        public ActionResult<SpectacleMagnificationModel> SpectacleMagnification(SpectacleMagnificationModel magnification)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            magnification.Result = Magnification.SpectacleMagnification(magnification.FrontBaseCurve, magnification.ActualLensPower, magnification.Index, magnification.Thickness, magnification.VertexDistance);
+            return magnification;
+        }
+    }
 }
